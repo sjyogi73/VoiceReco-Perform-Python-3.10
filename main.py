a9d13bff85db7e18fb6768fe0d18be321a5fc483
sjyogi73 <sjyogi72@gmail.com>
2022-08-24 13:51:21 +0530

    Add Features in Project 1.Wikipedia 2.pyJokes 3.pyWhatKit

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.say('Im your Alexa')
-engine.say('What can Do for you')
+engine.say('Im your yogi ...how can i help you')
 engine.runAndWait()
 
 
@@ -14,18 +18,52 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def take_command():
-try:
-    with sr.Microphone() as source:
-        print('Listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
 
-        if 'alexa' in command:
-            print(command)
-except:
-    pass
-return command
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
 
 
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%H:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+
+    elif 'who is the' in command:
+        person = command.replace('who is the',  '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'are you single' in command:
+        talk('no,Im committed with my boss')
+    elif 'love you' in command:
+        talk('im loving ... my dear')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+        print(pyjokes.get_joke())
+    else:
+        talk('Hey whats your saying ....i could not understand')
+
+
+while True:
+    run_alexa()
+
+run_alexa()
